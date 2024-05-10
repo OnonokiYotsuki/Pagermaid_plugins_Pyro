0bf7b956ca57e6d0f8c1f998499aa5277ef103e8
OnonokiYotsuki <defyhcb@gmail.com>
2024-05-10 14:08:02 +0800

    删除无用的插件

--- memeGenerator/main.py
+++
@@ -1,8 +0,0 @@
-import aiohttp
-import io
-from pagermaid.listener import listener
-from pagermaid.enums import Message, AsyncClient
-
-@listener(command="ask", description="发送请求并回复图片")
-async def ask(message: Message, client: AsyncClient):
-    await message.edit("正在ask...")
