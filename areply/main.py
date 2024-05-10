65029009177dad073354d106a657deb142c1fca3
OnonokiYotsuki <defyhcb@gmail.com>
2024-05-10 14:13:48 +0800

    删除无用插件

--- areply/main.py
+++
@@ -1,8 +0,0 @@
-import aiohttp
-import io
-from pagermaid.listener import listener
-from pagermaid.enums import Message, AsyncClient
-
-@listener(filters.text & filters.regex(r"\($"))
-async def reply_bracket_close(_, message: Message):
-    await message.reply(message.chat.id, ")")
